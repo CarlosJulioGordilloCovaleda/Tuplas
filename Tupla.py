492ca687c60cd7e723cabb9fd92095f574ea92e1
CarlosJulioGordilloCovaleda <160853185+CarlosJulioGordilloCovaleda@users.noreply.github.com>
2024-02-26 09:07:49 -0500

    Ejercisio Tupla

---
+++ Tupla.py
@@ -0,0 +1,14 @@
+#Informaciòn de 
+#DIreccciòn
+#Educaciòn
+#Informaciòn Laboral
+
+nombre=input("Ingrese su nombre: ")
+Edad=input("Ingrese su edad: ")
+Dir=input("Ingrese su direcciòn : ")
+Edu=input("Que educaciòn usted tiene : ")
+Ila=input("Tiene experiencia laboral en progrmaciòn si o no: ")
+
+var=tuple((nombre,Edad,Dir,Edu,Ila))
+
+print(var)   
